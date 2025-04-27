e9e33006de8b3d1f75c3bbd55bcce687d07b389b
jun <an0yjun@naver.com>
2025-04-27 16:13:40 +0900

    phzzk userservice 최초 생성

---
+++ src/main/java/phzzk/phzzkhexagonaluserservice/PhzzkHexagonalUserServiceApplication.java
@@ -0,0 +1,13 @@
+package phzzk.phzzkhexagonaluserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhzzkHexagonalUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhzzkHexagonalUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/phzzk/phzzkhexagonaluserservice/PhzzkHexagonalUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package phzzk.phzzkhexagonaluserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhzzkHexagonalUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
